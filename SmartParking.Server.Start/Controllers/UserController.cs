1bf230904c3e6de48485039660ec864d996a0a90
zzp229 <13556632276@163.com>
2024-04-03 18:55:35 +0800

    添加项目文件。

---
+++ SmartParking.Server.Start/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartParking.Server.Start.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
